bd7b68e3145c07311c14507e1b50ccd3a3288f9c
Umabalan Nagarajan <balan2010@users.noreply.github.com>
2017-11-17 13:02:41 +1100

    Removed auto queue creator and passing in queueurl

--- sqs_listener/__init__.py
+++ sqs_listener/__init__.py
@@ -1,16 +1,3 @@
-"""
-script for running sqs listener
-
-Created December 21st, 2016
-@author: Yaakov Gesher
-@version: 0.2.3
-@license: Apache
-"""
-
-# ================
-# start imports
-# ================
-
 import boto3
 import boto3.session
 import json
@@ -21,28 +8,20 @@ import sys
 from sqs_launcher import SqsLauncher
 from abc import ABCMeta, abstractmethod
 
-# ================
-# start class
-# ================
-
 sqs_logger = logging.getLogger('sqs_listener')
 
 class SqsListener(object):
     __metaclass__ = ABCMeta
 
     def __init__(self, queue, **kwargs):
-        """
-        :param queue: (str) name of queue to listen to
-        :param kwargs: error_queue=None, interval=60, visibility_timeout='600', error_visibility_timeout='600', force_delete=False
-        """
         if not os.environ.get('AWS_ACCOUNT_ID', None):
             raise EnvironmentError('Environment variable `AWS_ACCOUNT_ID` not set')
-        self._queue_name = queue
+        self._queue_name = None
         self._poll_interval = kwargs["interval"] if 'interval' in kwargs else 60
         self._queue_visibility_timeout = kwargs['visibility_timeout'] if 'visibility_timeout' in kwargs else '600'
         self._error_queue_name = kwargs['error_queue'] if 'error_queue' in kwargs else None
         self._error_queue_visibility_timeout = kwargs['error_visibility_timeout'] if 'error_visibility_timeout' in kwargs else '600'
-        self._queue_url = None
+        self._queue_url = queue
         self._message_attribute_names = kwargs['message_attribute_names'] if 'message_attribute_names' in kwargs else []
         self._attribute_names = kwargs['attribute_names'] if 'attribute_names' in kwargs else []
         self._force_delete = kwargs['force_delete'] if 'force_delete' in kwargs else False
@@ -52,62 +31,8 @@ class SqsListener(object):
 
 
     def _initialize_client(self):
-        #sqs = boto3.client('sqs', region_name=self._region_name)
-
-        # new session for each instantiation
         self._session = boto3.session.Session()
         sqs = self._session.client('sqs', region_name=self._region_name)
-
-        queues = sqs.list_queues()
-        mainQueueExists = False
-        errorQueueExists = False
-        if 'QueueUrls' in queues:
-            for q in queues['QueueUrls']:
-                qname = q.split('/')[-1]
-                if qname == self._queue_name:
-                    mainQueueExists = True
-                if self._error_queue_name and qname == self._error_queue_name:
-                    errorQueueExists = True
-
-
-        # create queue if necessary. 
-        # creation is idempotent, no harm in calling on a queue if it already exists.
-        if not mainQueueExists:
-            sqs_logger.warning("main queue not found, creating now")
-
-            # is this a fifo queue?
-            if self._queue_name.endswith(".fifo"):
-                fifoQueue="true"
-                q = sqs.create_queue(
-                    QueueName=self._queue_name,
-                    Attributes={
-                        'VisibilityTimeout': self._queue_visibility_timeout,  # 10 minutes
-                        'FifoQueue':fifoQueue
-                    }
-                )
-            else:
-                # need to avoid FifoQueue property for normal non-fifo queues
-                q = sqs.create_queue(
-                    QueueName=self._queue_name,
-                    Attributes={
-                        'VisibilityTimeout': self._queue_visibility_timeout,  # 10 minutes
-                    }
-                )
-            self._queue_url = q['QueueUrl']
-
-        if self._error_queue_name and not errorQueueExists:
-            sqs_logger.warning("error queue not found, creating now")
-            q = sqs.create_queue(
-                QueueName=self._error_queue_name,
-                Attributes={
-                    'VisibilityTimeout': self._queue_visibility_timeout  # 10 minutes
-                }
-            )
-
-        else:
-            qs = sqs.get_queue_url(QueueName=self._queue_name,
-                                   QueueOwnerAWSAccountId=os.environ.get('AWS_ACCOUNT_ID', None))
-            self._queue_url = qs['QueueUrl']
         return sqs
 
     def _start_listening(self):
@@ -189,11 +114,4 @@ class SqsListener(object):
 
     @abstractmethod
     def handle_message(self, body, attributes, messages_attributes):
-        """
-        Implement this method to do something with the SQS message contents
-        :param body: dict
-        :param attributes: dict
-        :param messages_attributes: dict
-        :return:
-        """
         return
